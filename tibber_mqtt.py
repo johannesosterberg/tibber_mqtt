a4e0633e9ae0f61cce74be03e5eae1b901b57edc
johannesosterberg <info@johannesosterberg.de>
2023-03-04 21:52:39 +0100

    Create tibber_mqtt.py

---
+++ tibber_mqtt.py
@@ -0,0 +1,49 @@
+#python 3.9
+#Tibber lib tibber.py https://github.com/BeatsuDev/tibber.py
+import time
+import tibber
+from paho.mqtt import client as mqtt_client
+
+#Tibber API Token find unnder https://developer.tibber.com/
+ACCESS_TOKEN = ""
+
+#MQTT Broker Settings
+broker = "ipadress"
+port = 1883
+client_id = 'Tibber_MQTT'
+username = "change"
+password = "change"
+
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+          print("Connected to MQTT Broker!")
+        else:
+          print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+account = tibber.Account(ACCESS_TOKEN)
+home = account.homes[0]
+    
+@home.event("live_measurement") 
+async def process_data(data):
+  print("consumption:",data.power, "Wh")
+  tibberpower = client.publish(topic='tibber/power1',payload=data.power,qos=1)
+  tibberpower.wait_for_publish()
+  print("MQTT is send:",tibberpower.is_published())
+  time.sleep(3)
+    
+def when_to_stop(data):
+  return False
+
+client = connect_mqtt()
+client.loop_start()
+home.start_live_feed(user_agent = "UserAgent/0.0.1", exit_condition = when_to_stop)
+
+
